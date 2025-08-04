05bf66be2f57ad14affa200b0c2f8c35afde88c3
MyTurnMyBrain <caobojun@cnu.edu.cn>
2025-08-04 10:21:00 +0800

    Update manager_sac_agent.py

--- manager_sac_agent.py
+++ manager_sac_agent.py
@@ -6,9 +6,7 @@ def _load_module(name, path):
     spec.loader.exec_module(module)
     return module
 
-# 加载 pyc 模块
 sac_agent0 = _load_module("sac_agent", "sac_agent.pyc")
 
-# 导出你希望暴露的内容
 sac_agent = sac_agent0.sac_agent
 
