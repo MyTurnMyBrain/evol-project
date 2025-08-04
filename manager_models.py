45d4d9915c9d7cd5158199fa87a73d94156ba642
MyTurnMyBrain <caobojun@cnu.edu.cn>
2025-08-04 10:20:38 +0800

    Update manager_models.py

--- manager_models.py
+++ manager_models.py
@@ -6,9 +6,7 @@ def _load_module(name, path):
     spec.loader.exec_module(module)
     return module
 
-# 加载 pyc 模块
 models = _load_module("models", "models.pyc")
 
-# 导出你希望暴露的内容
 actor_critic_net = models.actor_critic_net
 
